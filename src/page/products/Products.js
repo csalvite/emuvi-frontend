312fdf8d752eee30d1f99ae9f0c237b2d7636d77
csalvite <csalvite@gmail.com>
2022-01-15 13:22:05 +0100

    structuring products page

--- src/page/products/Products.js
+++ src/page/products/Products.js
@@ -39,13 +39,33 @@ function Products() {
   }, []);
 
   return (
-    <div className='products'>
-      <div>Página de Productos</div>
+    <div className='list-products'>
+      <div>
+        <h1>Página de Productos</h1>
+      </div>
       <div>
         {error ? (
           <div style={{ color: 'red' }}>Ha habido un error</div>
         ) : (
-          products.map((product) => <div key={product.id}>{product.name}</div>)
+          products.map((product) => {
+            return (
+              <div key={product.id} className='product'>
+                <h2>{product.name}</h2>
+                <h3>{product.price}€</h3>
+                <p>{product.category}</p>
+                <p>
+                  Producto creado el:{' '}
+                  {new Date(product.createdAt).toLocaleDateString()}
+                </p>
+                {product.photos.map((photo) => (
+                  <img
+                    src={`${REACT_APP_LOCALHOST}/photos/${photo.name}`}
+                    alt='product_photo'
+                  />
+                ))}
+              </div>
+            );
+          })
         )}
       </div>
     </div>
