398afcd8b85590db3a7de0f5e0ad78d63f9ac5a7
marcomgcal <marcomgcal@gmail.com>
2022-01-27 18:42:25 +0100

    about us added

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,54 +4,83 @@ import './AboutUs.css';
 function AboutUs() {
   return (
     <>
+      <h1>NUESTRO EQUIPO</h1>
       <div className="container">
-        <h3>NUESTRO EQUIPO</h3>
         <div className="card">
           <div className="imgBox">
-            <img src="./resources/images/marco.png" alt="marco profile"></img>
+            <img
+              src="./resources/images/marco-min.png"
+              alt="marco profile"
+            ></img>
           </div>
           <div className="details">
-            <h3>Lorem Lorem Macu</h3>
+            <h4>Marco Muñoz</h4>
+            <h5>Especialista en...</h5>
             <p>
               Aquí iría una breve presentación de cada uno de nosotros, así como
               alguna afición o destreza a destacar
             </p>
+
+            <a href="https://www.linkedin.com/in/marco-m-b33a51191/">
+              <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="https://www.facebook.com/marco.munozgormedino/">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/marco-m-b33a51191/">
+              <i class="fab fa-instagram-square"></i>
+            </a>
           </div>
         </div>
         <div className="card">
           <div className="imgBox">
-            <img src="./resources/images/marco.png" alt="marco profile"></img>
+            <img
+              src="./resources/images/cesar-min.png"
+              alt="cesar profile"
+            ></img>
           </div>
           <div className="details">
-            <h2>Lorem Lorem Macu</h2>
+            <h4>Cesar Alvite</h4>
+            <h5>Especialista en...</h5>
             <p>
               Aquí iría una breve presentación de cada uno de nosotros, así como
               alguna afición o destreza a destacar
             </p>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-instagram-square"></i>
           </div>
         </div>
         <div className="card">
           <div className="imgBox">
-            <img src="./resources/images/marco.png" alt="marco profile"></img>
+            <img src="./resources/images/alex-min.png" alt="alex profile"></img>
           </div>
           <div className="details">
-            <h2>Lorem Lorem Macu</h2>
+            <h4>Alex Coba</h4>
+            <h5>Especialista en...</h5>
             <p>
               Aquí iría una breve presentación de cada uno de nosotros, así como
               alguna afición o destreza a destacar
             </p>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-instagram-square"></i>
           </div>
         </div>
         <div className="card">
           <div className="imgBox">
-            <img src="./resources/images/marco.png" alt="marco profile"></img>
+            <img src="./resources/images/dani-min.png" alt="dani profile"></img>
           </div>
           <div className="details">
-            <h2>Lorem Lorem Macu</h2>
+            <h4>Dani Seco</h4>
+            <h5>Especialista en...</h5>
             <p>
               Aquí iría una breve presentación de cada uno de nosotros, así como
               alguna afición o destreza a destacar
             </p>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-instagram-square"></i>
           </div>
         </div>
       </div>
