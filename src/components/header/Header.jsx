b3a2a4ff6cb55140eaafd2c4bae8d1e35c81dbe1
César Alvite <78419782+csalvite@users.noreply.github.com>
2022-02-03 17:27:10 +0100

    Add files via upload

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { LoginButton } from '../../components/users/Login';
+import { LogoutButton } from '../../components/users/Logout';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Link } from 'react-router-dom';
+import { ProfileHeader } from '../../components/users/ProfileHeader';
+
+import './Header.css';
+
+function Header1() {
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <div>
+      <header>
+        <video autoPlay loop muted src="./resources/videos/HeaderVideoBG.mp4">
+          Video cannot be loaded :(
+        </video>
+        <nav>
+          <a href="#" className="logo">
+            <i className="fas fa-globe-africa"></i>EMUVI
+          </a>
+          <div className="search_box">
+            <input type="search" placeholder="Encontrar algo..." />
+            <span className="fa fa-search"></span>
+          </div>
+
+          {isAuthenticated ? (
+            <>
+              <ProfileHeader />
+              <LogoutButton />
+            </>
+          ) : (
+            <LoginButton />
+          )}
+        </nav>
+
+        <div className="header-container">
+          <h2>EL MUNDO VINTAGE</h2>
+          <h3>COMPRA-VENTA DE ARTÍCULOS RETRO</h3>
+          <Link to="/products">
+            <button className="products-button">PRODUCTOS</button>
+          </Link>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header1;
