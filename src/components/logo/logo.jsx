e891180f313f625976304b0ecc301b9924c3d2ee
marcomgcal <marcomgcal@gmail.com>
2022-02-22 18:00:10 +0100

    some changes

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './logo.css';
+
+const Logo = () => {
+	return (
+		<section className="logo-section">
+			<a href="#" className="logo">
+				<i id="globe-icon" className="fas fa-globe-africa"></i>EMUVI{' '}
+				<p className="Rreg">®</p>
+			</a>
+		</section>
+	);
+};
+
+export default Logo;
