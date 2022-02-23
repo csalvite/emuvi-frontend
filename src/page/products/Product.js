ba0f866963f896c55991eff2777d1ae426f7d036
alexcoba <alexcobavazquez.hotmail.com>
2022-02-23 19:53:21 +0100

    add map in product.js

--- src/page/products/Product.js
+++ src/page/products/Product.js
@@ -5,22 +5,17 @@ import JustNav from '../../components/justNavHeader/JustNav';
 import { ButtonNewOffer } from '../../components/offers/ButtonNewOffer';
 import ProductPhoto from '../../components/products/ProductPhoto';
 import ProductSingleData from '../../components/products/ProductSingleData';
-<<<<<<< HEAD
 import UserMapProduct from '../../components/products/UserMapProduct';
-=======
-import { usePrivateUser } from '../../hooks/usePrivateUser';
->>>>>>> 23a030969efc359bf80c5559198112a5394128dd
 import useProductDetail from '../../hooks/useProductDetail';
 import './Product.css';
 // Página Producto en Detalle
 const Product = (props) => {
-  const [user] = usePrivateUser();
+  // const [user] = usePrivateUser();
   const { product } = useProductDetail();
 
   return (
     <>
       <JustNav />
-<<<<<<< HEAD
 
       <ProductPhoto />
       <ProductSingleData />
@@ -28,12 +23,6 @@ const Product = (props) => {
       <ButtonNewOffer idProduct={product.id} idUser={product.idUser} />
       <div className='leaflet-container'>
         <UserMapProduct />
-=======
-      <div id='productpage'>
-        <ProductPhoto />
-        <ProductSingleData />
-        <ButtonNewOffer idProduct={product.id} idUser={user.id} />
->>>>>>> 23a030969efc359bf80c5559198112a5394128dd
       </div>
       <Footer />
     </>
