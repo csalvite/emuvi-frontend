ff0ed75e0398fb87a15deeaebb333faeab646e90
marcomgcal <marcomgcal@gmail.com>
2022-02-24 21:20:43 +0100

    main carousel modfied

--- src/components/latestProducts/LatestProducts.jsx
+++ src/components/latestProducts/LatestProducts.jsx
@@ -3,55 +3,12 @@ import { Link } from 'react-router-dom';
 import './Carousel.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCards } from 'swiper';
-import { ButtonNewOffer } from '../offers/ButtonNewOffer';
 
 const { REACT_APP_LOCALHOST } = process.env;
 
 const LatestProducts = () => {
 	const { products, loading, error } = useLatestProducts();
 
-<<<<<<< HEAD
-  if (loading) return 'Cargando....';
-  if (error) return <div>{error}</div>;
-  return products.length > 0 ? (
-    <div className='product-list'>
-      <h3>PRODUCTOS MÁS NOVEDOSOS:</h3>
-      <ul className='containerlist'>
-        <Swiper
-          effect={'cards'}
-          grabCursor={true}
-          modules={[EffectCards]}
-          className='mySwiper'
-        >
-          {products.map((product) => (
-            <SwiperSlide key={product.id}>
-              <Link to={`/products/${product.id}`}>
-                <div>
-                  {/* {product.photos.map((photo, index) => ( */}
-                  <img
-                    src={
-                      product.photos.length > 0
-                        ? `${REACT_APP_LOCALHOST}/avatar/${product.photos[0].name}`
-                        : '/resources/images/product-photo-not-found.jpg'
-                    }
-                    alt='product_photo'
-                    style={{ width: '100%' }}
-                  />
-                  {/* ))} */}
-                </div>
-                <h2>{product.name}</h2>
-                <h3>{product.price}</h3>
-              </Link>
-              <ButtonNewOffer idProduct={product.id} idUser={product.idUser} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </ul>
-    </div>
-  ) : (
-    <div>No hay productos</div>
-  );
-=======
 	if (loading) return 'Cargando....';
 	if (error) return <div>{error}</div>;
 	return products.length > 0 ? (
@@ -90,7 +47,6 @@ const LatestProducts = () => {
 	) : (
 		<div>No hay productos</div>
 	);
->>>>>>> Marco
 };
 
 export default LatestProducts;
