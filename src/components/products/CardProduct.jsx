eae1af0fe578cdce797f6eaec4d6f1d11ff22cc0
marcomgcal <marcomgcal@gmail.com>
2022-02-25 17:14:19 +0100

    products slyling

--- src/components/products/CardProduct.jsx
+++ src/components/products/CardProduct.jsx
@@ -4,32 +4,32 @@ import { Link } from 'react-router-dom';
 const { REACT_APP_LOCALHOST } = process.env;
 
 export default function CardProduct(props, product) {
-  return (
-    <>
-      <div className='cardproduct'>
-        <div className='card__body'>
-          <Link to={`/products/${props.product.id}`}>
-            <img
-              className='card__img'
-              src={
-                props.product.photos.length > 0
-                  ? `${REACT_APP_LOCALHOST}/avatar/${props.product.photos[0].name}`
-                  : '/resources/images/product-photo-not-found.jpg'
-              }
-              alt='list_product'
-            />
+	return (
+		<>
+			<div className="cardproduct">
+				<div className="card__body">
+					<Link to={`/products/${props.product.id}`}>
+						<img
+							className="card__img"
+							src={
+								props.product.photos.length > 0
+									? `${REACT_APP_LOCALHOST}/avatar/${props.product.photos[0].name}`
+									: '/resources/images/product-photo-not-found.jpg'
+							}
+							alt="list_product"
+						/>
 
-            <h4 className='card__title'>{props.product.name}</h4>
-            <h4 className='card__price'>{props.product.price}</h4>
+						<h4 className="card__title">{props.product.name}</h4>
+						<h4 className="card__price">{props.product.price}</h4>
 
-            <p className='card__description'>{props.product.description}</p>
-          </Link>
-        </div>
-        <ButtonNewOffer
-          idProduct={props.product.id}
-          idUser={props.product.idUser}
-        />
-      </div>
-    </>
-  );
+						{/*<p className='card__description'>{props.product.description}</p>*/}
+					</Link>
+				</div>
+				{/*<ButtonNewOffer
+					idProduct={props.product.id}
+					idUser={props.product.idUser}
+				/>*/}
+			</div>
+		</>
+	);
 }
