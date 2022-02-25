f53a7a021573b0a2d37c873be48618af7de7bb6a
Danutty <danielsecolozano@gmail.com>
2022-02-25 19:27:41 +0100

    loading modificado

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -4,20 +4,10 @@ import React from 'react';
 
 const LoadingComponent= () => {
     return (
-        <div>
-            <section className='imagenLoading'>
-            <video
-                autoPlay
-                loop
-                muted
-                src="./resources/videos/loadingEmuvi.mp4"
-                className="VLoading">
-                Video cannot be loaded :(
-                </video>
-                <h2>Cargando datos solicitados...</h2>
-                
-</section>
-
+        <div className='loadingAnimation'>
+            <div className='center'></div>
+                <section className= 'ring'></section>
+                <span className='cargando'>Cargando...</span>
         </div>
     );
 }
