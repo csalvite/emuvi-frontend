e4cf5408226405154d4cd3b51edbe323d413202b
marcomgcal <marcomgcal@gmail.com>
2022-02-27 12:22:37 +0100

    styling main carousel

--- src/components/latestProducts/LatestProducts.jsx
+++ src/components/latestProducts/LatestProducts.jsx
@@ -31,7 +31,7 @@ const LatestProducts = () => {
 					navigation={true}
 					effect={'coverflow'}
 					centeredSlides={true}
-					slidesPerView={window.innerWidth < 768 ? 1 : 'auto'}
+					slidesPerView={3}
 					loop={true}
 					coverflowEffect={{
 						rotate: 50,
