c35091503a44ae9dbc0f55a1f692fac538f2cfce
marcomgcal <marcomgcal@gmail.com>
2022-03-02 11:12:04 +0100

    styling products component v1

--- src/components/products/Buttonproducts.jsx
+++ src/components/products/Buttonproducts.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 function Buttonproducts({ prevPage, nextPage }) {
-  return (
-    <div id='pagination_container'>
-      <button className='btn' onClick={prevPage}>
-        Anteriores
-      </button>
-      &nbsp;
-      <button className='btn' onClick={nextPage}>
-        Siguientes
-      </button>
-    </div>
-  );
+	return (
+		<div id="pagination_container">
+			<button className="prev-btn" onClick={prevPage}>
+				Anteriores
+			</button>
+			&nbsp;
+			<button className="next-btn" onClick={nextPage}>
+				Siguientes
+			</button>
+		</div>
+	);
 }
 
 export default Buttonproducts;
--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -9,33 +9,36 @@ import Buttonproducts from './Buttonproducts';
 // const { REACT_APP_LOCALHOST } = process.env;
 
 const ProductList = () => {
-  const [currentPage, setCurrentPage] = useState(0);
-  const { products, loading, error } = useProducts();
-  if (loading) return 'Cargando....';
-  if (error) return <div>{error}</div>;
-  const productFiltered = () => {
-    return products.slice(currentPage, currentPage + 8);
-  };
+	const [currentPage, setCurrentPage] = useState(0);
+	const { products, loading, error } = useProducts();
+	if (loading) return 'Cargando....';
+	if (error) return <div>{error}</div>;
+	const productFiltered = () => {
+		return products.slice(currentPage, currentPage + 8);
+	};
 
-  const nextPage = () => {
-    setCurrentPage(currentPage + 8);
-  };
-  const prevPage = () => {
-    if (currentPage > 0) setCurrentPage(currentPage - 8);
-  };
+	const nextPage = () => {
+		setCurrentPage(currentPage + 8);
+	};
+	const prevPage = () => {
+		if (currentPage > 0) setCurrentPage(currentPage - 8);
+	};
 
-  return products.length > 0 && !products.sold ? (
-    <>
-      <ul className='container_productlist' style={{ listStyleType: 'none' }}>
-        {productFiltered().map((product, index) => {
-          return <CardProduct product={product} key={product.id} />;
-        })}
-      </ul>
-      <Buttonproducts nextPage={nextPage} prevPage={prevPage} />
-    </>
-  ) : (
-    <div>No hay productos</div>
-  );
+	return products.length > 0 && !products.sold ? (
+		<>
+			<section className="products-section">
+				<h1 className="products-main-title">PRODUCTOS</h1>
+				<ul className="container_productlist" style={{ listStyleType: 'none' }}>
+					{productFiltered().map((product, index) => {
+						return <CardProduct product={product} key={product.id} />;
+					})}
+				</ul>
+				<Buttonproducts nextPage={nextPage} prevPage={prevPage} />
+			</section>
+		</>
+	) : (
+		<div>No hay productos</div>
+	);
 };
 
 export default ProductList;
