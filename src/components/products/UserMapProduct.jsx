7bbb67641369d5ad3c1199aa4275eee5296b0241
alexcoba <alexcobavazquez.hotmail.com>
2022-03-04 17:55:57 +0100

    Solution new error map

--- src/components/products/UserMapProduct.jsx
+++ src/components/products/UserMapProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 // import { Icon } from 'leaflet';
 // import './UserMapProduct.css';
