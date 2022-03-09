5bd7b60d79e91ebaf32602bd03a44334a5a814e8
marcomgcal <marcomgcal@gmail.com>
2022-03-09 17:37:24 +0100

    styling public user v-1

--- src/components/publicUser/PublicUserInfo.jsx
+++ src/components/publicUser/PublicUserInfo.jsx
@@ -1,31 +1,47 @@
-import { Avatar, Rating } from "@mui/material";
+import { Avatar, Rating } from '@mui/material';
+import './PublicUserInfo.css';
 
 const { REACT_APP_LOCALHOST } = process.env;
 
 export const PublicUserInfo = ({ publicUser }) => {
-
-    return (
-        <div className="public-user-info">
-            <Avatar
-                alt={publicUser.name}
-                src={
-                `${REACT_APP_LOCALHOST}/avatar/${publicUser.avatar}`
-                }
-                sx={{ width: "10rem", height: "10rem" }}
-            /> 
-            <Rating
-                name='size-large'
-                value={publicUser.mediaVotes}
-                size='large'
-                readOnly
-            />
-            <h2>{publicUser.name} {publicUser.lastname}</h2>
-            <h4>Biografía: </h4>
-            <p>{publicUser.biography}</p>
-            <h4>Info de contacto: </h4>
-            <p>Teléfono: {publicUser.phone}</p>
-            <p>Email: {publicUser.email}</p>
-            <p>{publicUser.city}, {publicUser.province}</p>
-        </div>
-    );
-}
+	return (
+		<>
+			<div className="public-user-stars-name">
+				<Avatar
+					className="public-profile-avatar"
+					alt={publicUser.name}
+					src={`${REACT_APP_LOCALHOST}/avatar/${publicUser.avatar}`}
+					sx={{ width: '10rem', height: '10rem' }}
+				/>
+				<Rating
+					className="public-profile-stars"
+					name="size-large"
+					value={publicUser.mediaVotes}
+					size="large"
+					readOnly
+				/>
+				<h2 className="public-profile-name">
+					{publicUser.name} {publicUser.lastname}
+				</h2>
+			</div>
+			<div className="public-user-more-info">
+				<p>
+					<i className="fa-solid fa-phone public-profile-icons"></i>{' '}
+					{publicUser.phone}
+				</p>
+				<p>
+					<i className="fa-solid fa-envelope public-profile-icons"></i>{' '}
+					{publicUser.email}
+				</p>
+				<p>
+					<i className="fa-solid fa-location-dot public-profile-icons"></i>{' '}
+					{publicUser.city}, {publicUser.province}
+				</p>
+			</div>
+			<div className="public-user-bio">
+				<h4>Biografía: </h4>
+				<p>{publicUser.biography}</p>
+			</div>
+		</>
+	);
+};
