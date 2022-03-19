c1b21645d33b98e5208c4735b623bf17625e5a96
marcomgcal <marcomgcal@gmail.com>
2022-03-19 19:02:32 +0100

    meeting popup styled

--- src/components/offers/FormAcceptOffer.jsx
+++ src/components/offers/FormAcceptOffer.jsx
@@ -1,95 +1,122 @@
-import { IconButton, Snackbar } from "@mui/material";
-import { useContext, useState } from "react";
-import { TokenContext } from "../..";
-import "../popUp/acceptStyle.css";
+import { IconButton, Snackbar } from '@mui/material';
+import { useContext, useState } from 'react';
+import { TokenContext } from '../..';
+import '../popUp/acceptStyle.css';
 
 const { REACT_APP_LOCALHOST } = process.env;
 
-export const FormAcceptOffer = ({ setShowPopUp, idUser, idOffer, reserveStatus, offers, setOffers }) => {
+export const FormAcceptOffer = ({
+	setShowPopUp,
+	idUser,
+	idOffer,
+	reserveStatus,
+	offers,
+	setOffers,
+}) => {
+	const [token] = useContext(TokenContext);
+	const [loading, setLoading] = useState(false);
+	const [state, setState] = useState();
 
-    const [token] = useContext(TokenContext);
-    const [loading, setLoading] = useState(false);
-    const [state, setState] = useState();   
+	const handleAcceptOffer = async (e) => {
+		e.preventDefault();
 
-    const handleAcceptOffer = async (e) => {
-        e.preventDefault();
+		const offerData = {
+			street: e.target.elements.street.value,
+			city: e.target.elements.city.value,
+			time: e.target.elements.time.value,
+			date: e.target.elements.date.value,
+		};
 
-        const offerData = {
-            street: e.target.elements.street.value,
-            city: e.target.elements.city.value,
-            time: e.target.elements.time.value,
-            date: e.target.elements.date.value,
-        }
-        
-        try {
-            const url = `${REACT_APP_LOCALHOST}/users/${idUser}/offers/${idOffer}/accept`;
-            
-            const response = await fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(offerData),
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: token.token,
-                }
-            });
+		try {
+			const url = `${REACT_APP_LOCALHOST}/users/${idUser}/offers/${idOffer}/accept`;
 
-            setLoading(true);
+			const response = await fetch(url, {
+				method: 'POST',
+				body: JSON.stringify(offerData),
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: token.token,
+				},
+			});
 
-            if (response.ok) {
-                const body = await response.json();
-                const arrNew = offers.filter((offer) => offer.id !== Number(idOffer));
-                setOffers(arrNew);
-                setState(body.message);
-            } else {
-                if (offerData.street === '' || offerData.city === '' || offerData.time === '' || offerData.date === '') {
-                    setState('Debes indicar todos los datos para la reunión con el comprador.');
-                }
-                
-                if (reserveStatus === 'aceptada' || reserveStatus === 'denegada') {
-                    setState('Ya has tomado una decisión sobre esta oferta, no puedes cambiar su estado.');
-                }
+			setLoading(true);
 
-                console.error('Hubo un error al aceptar la oferta');
-            }
+			if (response.ok) {
+				const body = await response.json();
+				const arrNew = offers.filter((offer) => offer.id !== Number(idOffer));
+				setOffers(arrNew);
+				setState(body.message);
+			} else {
+				if (
+					offerData.street === '' ||
+					offerData.city === '' ||
+					offerData.time === '' ||
+					offerData.date === ''
+				) {
+					setState(
+						'Debes indicar todos los datos para la reunión con el comprador.',
+					);
+				}
 
-            setLoading(false);
+				if (reserveStatus === 'aceptada' || reserveStatus === 'denegada') {
+					setState(
+						'Ya has tomado una decisión sobre esta oferta, no puedes cambiar su estado.',
+					);
+				}
 
-        } catch (error) {
-            console.error(error.message);
-        }
-    }
+				console.error('Hubo un error al aceptar la oferta');
+			}
 
-    if (loading) {
-        return <h2>Cargando...</h2>
-    }
+			setLoading(false);
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
 
+	if (loading) {
+		return <h2>Cargando...</h2>;
+	}
 
-    return (
-        <div id="popup-background" className="accept-offer">
-            <form id="form-accept-offer" onSubmit={handleAcceptOffer}>
-                <span className="close-popup" onClick={() => setShowPopUp(false)}>X</span>
-                <h3>Rellena los datos para reunirte con el comprador</h3>
-                <ul>
-                    <li>
-                        <label htmlFor="street">Calle: </label>
-                        <input type='text' name='street' id="street" />
-                    </li>
-                    <li>
-                        <label htmlFor="city">Ciudad: </label>
-                        <input type='text' name='city' id="city" />
-                    </li>
-                    <li>
-                        <label htmlFor="time">Hora: </label>
-                        <input type='text' name='time' id="time" />
-                    </li>
-                    <li>
-                        <label htmlFor="date">Fecha: </label>
-                        <input type='date' name="date" id="date" />
-                    </li>
-                </ul>
-                <button className="btn">Aceptar Oferta</button>
-                {state ? <div>{state}</div> : ''}
-            </form>
-        </div>
-    )
-}
+	return (
+		<div id="popup-background" className="accept-offer">
+			<form id="form-accept-offer" onSubmit={handleAcceptOffer}>
+				<span
+					className="accept-offer-close-popup"
+					onClick={() => setShowPopUp(false)}>
+					X
+				</span>
+				<h3 className="accept-offer-text">
+					Rellena los datos para reunirte con el comprador
+				</h3>
+				<ul>
+					<li className="accept-offer-text">
+						<label className="accept-offer-label-text" htmlFor="street">
+							Calle:{' '}
+						</label>
+						<input type="text" name="street" id="street" />
+					</li>
+					<li className="accept-offer-text">
+						<label className="accept-offer-label-text" htmlFor="city">
+							Ciudad:{' '}
+						</label>
+						<input type="text" name="city" id="city" />
+					</li>
+					<li className="accept-offer-text">
+						<label className="accept-offer-label-text" htmlFor="time">
+							Hora:{' '}
+						</label>
+						<input type="text" name="time" id="time" />
+					</li>
+					<li className="accept-offer-text">
+						<label className="accept-offer-label-text" htmlFor="date">
+							Fecha:{' '}
+						</label>
+						<input type="date" name="date" id="date" />
+					</li>
+				</ul>
+				<button className="accept-offer-btn">Aceptar Oferta</button>
+				{state ? <div>{state}</div> : ''}
+			</form>
+		</div>
+	);
+};
