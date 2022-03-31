8a588b640fa640e5e2c9a732c8315c46ca9f62de
Danutty <danielsecolozano@gmail.com>
2022-03-31 16:49:29 +0200

    cambio de ruta volver 404

--- src/components/E404/E404.js
+++ src/components/E404/E404.js
@@ -5,28 +5,29 @@ import Footer from '../footer/Footer';
 import NoLandscape from '../../components/noLandscape/NoLandscape';
 
 const E404 = () => {
-	return (
-		<div className="E404">
-			<NoLandscape />
-			<Logo className="logo-404"></Logo>
-			<div className="Imagenn404">
-				<img
-					className="imagen404"
-					src="../resources/images/ninos404.png"
-					alt="Error 404"></img>
-				<div className="TextoYBoton">
-					<h1 className="h1-404">Ups... Error 404</h1>
-					<h2 className="h2-404">
-						Parece que tenemos problemas en encontrar eso
-					</h2>
-					<a href={`http://localhost:3000/`} className="logo">
-						<button className="products-button">Volver</button>
-					</a>
-				</div>
-			</div>
-			<Footer></Footer>
-		</div>
-	);
+  return (
+    <div className='E404'>
+      <NoLandscape />
+      <Logo className='logo-404'></Logo>
+      <div className='Imagenn404'>
+        <img
+          className='imagen404'
+          src='../resources/images/ninos404.png'
+          alt='Error 404'
+        ></img>
+        <div className='TextoYBoton'>
+          <h1 className='h1-404'>Ups... Error 404</h1>
+          <h2 className='h2-404'>
+            Parece que tenemos problemas en encontrar eso
+          </h2>
+          <a href={`https://www.emuviproject.net`} className='logo'>
+            <button className='products-button'>Volver</button>
+          </a>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default E404;
